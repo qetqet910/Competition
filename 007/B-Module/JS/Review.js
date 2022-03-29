5559c9780d4474702faded4927b64b4a1ea824f3
qetqet910 <79036088+qetqet910@users.noreply.github.com>
2022-03-29 17:18:32 +0900

    B-Module

--- 007/B-Module/JS/Review.js
+++ 007/B-Module/JS/Review.js
@@ -10,6 +10,13 @@ const Stars = document.querySelector("#stars");
 const StarsSpan = Stars.querySelector("span");
 const Des = document.querySelector("#des");
 const Img = document.querySelector(".img");
+const Cnt = document.querySelector(".cnt");
+const close = document.querySelector(".close");
+const addIMG = document.querySelector(".addIMG");
+const Sub = document.querySelector(".sub");
+
+Img.style.opacity = '0';
+Img.style.zIndex = '-999'
 
 ReviewBTN.addEventListener("click", (e) => {
     OnModal();
@@ -28,6 +35,50 @@ function OnModal(){
 Name.addEventListener("keyup", (e) => {
     e.target.value = e.target.value.replace(/[^ㄱ-힣a-zA-Z]/g, '');
 })
-Stars.addEventListener("mouseover", (e) => {
-    StarsSpan.style.width = `${e.target.value * 10}%`
+function StarsGO(e){
+    StarsSpan.style.width = `${e.value * 10}%`
+    Cnt.innerText = e.value;
+}
+close.addEventListener("click", (e) => {
+    setClose();
+    OffModal();
+})
+
+function setClose(){
+    Name.value = '';
+    Item.value = '';
+    Place.value = '';
+    Date.value = '';
+    Stars.value = 0;
+    StarsSpan.style.width = '0%';
+    Des.value = '';
+    Cnt.innerText = 0;
+    Img.value = '';
+    Img.style.opacity = '0';
+    Img.style.zIndex = '-999'
+}
+
+addIMG.addEventListener("click", (e) => {
+    e.preventDefault();
+    Img.style.opacity = '1';
+    Img.style.zIndex = '999'
+})
+
+function fileChecking(obj){
+    const extend = obj.value.substring(obj.value.lastIndexOf('.') + 1);
+    if(extend === 'jpg'){
+
+    }else{
+        alert("확장자 : JPG 파일만 첨부할 수 있습니다.")
+        obj.value = '';
+    }
+}
+
+Sub.addEventListener('click', (e) => {
+    if(Name.value.length > 1 && 50 > Name.value.length){
+        alert("­구매 후기가 등록되었습니다.")
+        OffModal();
+    }else{
+        alert("올바르지 않은 값입니다 다시 한 번 확인해주십시오.")
+    }
 })
